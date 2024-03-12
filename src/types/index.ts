6b5ff442a1ac08262f83ee73b5a282f983674d73
nishaj0 <njnishaj0@gmail.com>
2024-03-13 04:26:54 +0530

    feat: add first theme data

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Theme {
+   name: string;
+   background: {
+      primary: string;
+      secondary: string;
+   };
+   text: {
+      primary: string;
+      secondary: string;
+      tertiary: string;
+      error: string;
+   };
+}
---
+++ src/utils/theme.ts
@@ -0,0 +1,17 @@
+import { Theme } from '../types';
+
+export const themeColors: Record<string, Theme> = {
+   aura: {
+      name: 'Aura',
+      background: {
+         primary: '#15141b', // Black
+         secondary: '#3d375e', // Purple
+      },
+      text: {
+         primary: '#a277ff', // Purple lite
+         secondary: '#61ffca', // Green
+         tertiary: '#f694ff', // pink
+         error: '#ff6767', // red
+      },
+   },
+};

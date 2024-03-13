9d69f3dfbbb8fee7541c30a9cbae60d5feb2af03
nishaj0 <njnishaj0@gmail.com>
2024-03-13 17:26:31 +0530

    feat: add theme and font
    using Fira Code as font, and hard coded aura theme as theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { themeColors } from './utils/theme';
+import './App.css';
+
 function App() {
-   return <div>App</div>;
+   return (
+      <div
+         className="px-24 py-8 h-screen flex flex-col justify-between items-center"
+         style={{
+            backgroundColor: themeColors.aura.background.primary,
+            color: themeColors.aura.text.primary,
+         }}
+      >
+         <div>body</div>
+      </div>
+   );
 }
 
 export default App;

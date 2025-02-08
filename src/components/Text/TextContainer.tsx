423f2e5be6c5e5d7b24500c8789c0739b3b9f0b8
nishaj0 <njnishaj0@gmail.com>
2025-02-08 20:09:25 +0530

    feat: add focusing to `TextContainer`

--- src/components/Text/TextContainer.tsx
+++ src/components/Text/TextContainer.tsx
@@ -1,24 +1,44 @@
+import { useState } from 'react';
+
 interface TextContainerProps {
    textContent: string;
    className?: string;
 }
 
 function TextContainer({ textContent, className }: TextContainerProps) {
+   const [isFocused, setIsFocused] = useState<boolean>(false);
+
    let strArray: string[] = [];
    if (textContent) {
       strArray = textContent.split(' ');
    }
+
    return (
       <div
-         className={`flex flex-col justify-center items-center text-left text-2xl ${className}`}
+         // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+         tabIndex={0}
+         onFocus={() => setIsFocused(true)}
+         onBlur={() => setIsFocused(false)}
+         className={`flex flex-col justify-center items-center text-left text-2xl relative ${className}`}
       >
-         <p>
+         <p
+            className={`transition-opacity  ${!isFocused ? 'blur-sm opacity-70' : ''}`}
+         >
             {strArray.map((str, index) => (
                <>
-                  <span key={str + Math.random()}>{str}</span>
+                  <span>
+                     {str.split('').map((char) => (
+                        <span key={char + Math.random()}>{char}</span>
+                     ))}
+                  </span>
                   {index !== strArray.length - 1 && <span> </span>}
                </>
             ))}
+         </p>
+         <p
+            className={`absolute font-semibold text-3xl transition-opacity ${isFocused ? 'opacity-0' : ''}`}
+         >
+            Click here to start typing
          </p>
       </div>
    );

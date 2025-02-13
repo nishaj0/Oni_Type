cff3e01579618587fa82639af5c49fd3b4c3e755
nishaj0 <njnishaj0@gmail.com>
2025-02-14 02:38:46 +0530

    feat: update style when user typing text

--- src/components/Text/TextContainer.tsx
+++ src/components/Text/TextContainer.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useTheme } from '../../hooks';
 
 interface TextContainerProps {
    textContent: string;
@@ -7,33 +8,69 @@ interface TextContainerProps {
 
 function TextContainer({ textContent, className }: TextContainerProps) {
    const [isFocused, setIsFocused] = useState<boolean>(false);
+   const [typedText, setTypedText] = useState<string>('');
 
-   let strArray: string[] = [];
-   if (textContent) {
-      strArray = textContent.split(' ');
-   }
+   const { getTheme } = useTheme();
+   const currentThemeInfo = getTheme();
+
+   useEffect(() => {
+      const handleKeyDown = (event: KeyboardEvent) => {
+         if (isFocused) {
+            setTypedText((prev) => prev + event.key);
+         }
+      };
+
+      window.addEventListener('keydown', handleKeyDown);
+
+      return () => {
+         window.removeEventListener('keydown', handleKeyDown);
+      };
+   }, [isFocused]);
+
+   const handleFocus = () => {
+      setIsFocused(true);
+   };
+
+   const handleBlur = () => {
+      setIsFocused(false);
+   };
 
    return (
       <div
          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
          tabIndex={0}
-         onFocus={() => setIsFocused(true)}
-         onBlur={() => setIsFocused(false)}
-         className={`flex flex-col justify-center items-center text-left text-2xl relative ${className}`}
+         onFocus={handleFocus}
+         onBlur={handleBlur}
+         className={`flex flex-col justify-center items-center text-left text-2xl relative outline-none ${className}`}
       >
          <p
             className={`transition-opacity  ${!isFocused ? 'blur-sm opacity-70' : ''}`}
          >
-            {strArray.map((str, index) => (
-               <>
-                  <span>
-                     {str.split('').map((char) => (
-                        <span key={char + Math.random()}>{char}</span>
-                     ))}
-                  </span>
-                  {index !== strArray.length - 1 && <span> </span>}
-               </>
-            ))}
+            {textContent &&
+               textContent.split('').map((char, charIndex) => {
+                  const typedChar = typedText[charIndex];
+                  const isCorrect = char === typedChar;
+                  const style = isCorrect ? 'font-medium' : 'underline';
+                  const additionalStyle =
+                     typedChar && !isCorrect && char === ' '
+                        ? { backgroundColor: currentThemeInfo.errorText }
+                        : {};
+                  return (
+                     <span
+                        key={char + Math.random()}
+                        className={`opacity-50 ${typedChar ? `!opacity-100 ${style}` : ''}`}
+                        style={{
+                           ...additionalStyle,
+                           ...(typedChar && !isCorrect && char !== ' '
+                              ? { color: currentThemeInfo.errorText }
+                              : {}),
+                        }}
+                     >
+                        {char}
+                     </span>
+                  );
+               })}
+            {isFocused && <span className="blinking-cursor">|</span>}
          </p>
          <p
             className={`absolute font-semibold text-3xl transition-opacity ${isFocused ? 'opacity-0' : ''}`}
